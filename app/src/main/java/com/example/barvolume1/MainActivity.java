7825e64685fa15e5ca3ddd193badba9bd2bbfe65
danurwijayanto <mobinity.fx@gmail.com>
2019-12-16 00:31:55 +0700

    Add icon and add advertisement

--- app/src/main/java/com/example/barvolume1/MainActivity.java
+++ app/src/main/java/com/example/barvolume1/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView rvHeroes;
     private ArrayList<Hero> list = new ArrayList<>();
+    private AdView mAdView;
     private String title = "Perangkat Gamelan";
 
     @Override
@@ -24,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setActionBarTitle(title);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         rvHeroes = findViewById(R.id.rv_heroes);
         rvHeroes.setHasFixedSize(true);
